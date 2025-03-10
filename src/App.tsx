267abe414193837a45d238d927c21cd5bb2c70a2
Renan Gomes <renango2015@gmail.com>
2025-03-09 22:08:22 -0300

    feat(structure): update application structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import { Prototype } from './components/Prototype';
+import { Fragment } from 'react/jsx-runtime';
+import { Toaster } from './components/ui/sonner';
+import { Home } from './pages/Home';
 
 import '@/stylesheets/index.css';
 
 export default function App() {
-  return <Prototype />;
+  return (
+    <Fragment>
+      <Home />
+      <Toaster richColors theme='light' />
+    </Fragment>
+  );
 }
